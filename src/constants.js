213c2ce22eaec049594fe5b3d59b8a3eef1f8467
Victor Hidrogo <vhidrogo@uci.edu>
2025-06-04 09:54:41 -0700

    feat: Create constants file to centrally store range names

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const NAMED_RANGES = {
+    ApplicationView: {
+        APPLICATION_DETAIL_OUTPUTS: 'ApplicationView_ApplicationDetailOutputs',
+        CONSIDERATION_DETAIL_OUTPUTS: 'ApplicationView_ConsiderationDetailOutputs',
+        INTERVIEW_DOC_LINK: 'ApplicationView_InterviewDoc',
+        JOB_DESCRIPTION_LINK: 'ApplicationView_JobDescription',
+        LISTING_LINK: 'ApplicationView_ListingLink',
+        OUTCOME: 'ApplicationView_Outcome',
+        SEARCH_CRITERIA_INPUTS: 'ApplicationView_SearchCriteriaInputs',
+    }
+}
+
+module.exports = { NAMED_RANGES }
