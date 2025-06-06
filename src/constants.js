fcd404eaada8a2bf3ccc3807ab111deba49ecc87
Victor Hidrogo <vhidrogo@uci.edu>
2025-06-06 12:13:17 -0700

    feat: Add application view interview range names

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,9 @@
 const NAMED_RANGES = {
     ApplicationView: {
         APPLICATION_DETAIL_OUTPUTS: 'ApplicationView_ApplicationDetailOutputs',
+        APPLICATION_FUTURE_INTERVIEWS: 'ApplicationView_FutureInterviews',
+        APPLICATION_INTERVIEW_HEADERS: 'ApplicationView_InterviewHeaders',
+        APPLICATION_PAST_INTERVIEWS: 'ApplicationView_PastInterviews',
         CONSIDERATION_DETAIL_OUTPUTS: 'ApplicationView_ConsiderationDetailOutputs',
         INTERVIEW_DOC_LINK: 'ApplicationView_InterviewDoc',
         JOB_DESCRIPTION_LINK: 'ApplicationView_JobDescription',
